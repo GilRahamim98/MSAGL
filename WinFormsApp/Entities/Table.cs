0a10b10dd1eb935be85352674d9f3fc9e5b11e16
GilRahamim98 <95537455+GilRahamim98@users.noreply.github.com>
2024-01-22 17:22:33 +0200

    Update Table.cs

--- WinFormsApp/Entities/Table.cs
+++ WinFormsApp/Entities/Table.cs
@@ -1,12 +1,14 @@
-﻿namespace WinFormsApp.Entities
+namespace WinFormsApp.Entities
 {
     public class Table
     {
         public string TableName { get; set; }
         public string PrimaryKey { get; set; }
         public List<Tuple<string, Table>> ForeignKeys { get; set; }
+        public List<string> Columns { get; set; }
         public Table()
         {
+            
             ForeignKeys = new List<Tuple<string, Table>>();
         }
         public Table(string tableName)
@@ -20,5 +22,13 @@
             PrimaryKey = primaryKey;
             ForeignKeys = foreignKeys;
         }
+
+        public Table(string tableName, string primaryKey, List<string> columns, List<Tuple<string, Table>> foreignKeys)
+    {
+        TableName = tableName;
+        PrimaryKey = primaryKey;
+        Columns = columns;
+        ForeignKeys = foreignKeys;
+    }
     }
 }
